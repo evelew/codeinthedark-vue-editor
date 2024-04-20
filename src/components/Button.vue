d6e5885e53fb1c3018768a0e141c10062293a7ed
Evellyn Lima <evellyn.rosa@alice.com.br>
2024-04-20 01:58:41 -0300

    add style to the editor

---
+++ src/components/Button.vue
@@ -0,0 +1,36 @@
+<script setup>
+const props = defineProps({
+  size: {
+    type: String,
+    validator: (v) => ['small', 'large'].includes(v)
+  }
+})
+</script>
+
+<template>
+  <button class="button" :class="props.size">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.button {
+  background-color: rgba(58, 147, 100, 0.75);
+  color: #ffffff;
+  font-family: 'Press Start 2P';
+
+  &.large {
+    font-size: 56px;
+    padding: 20px;
+  }
+
+  &.small {
+    font-size: 12px;
+    padding: 10px 15px;
+  }
+
+  &:hover {
+    opacity: 0.8;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,120 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import CodeMirror from 'vue-codemirror6'
+import { lineNumbers, highlightActiveLineGutter } from '@codemirror/view'
+import { Compartment } from '@codemirror/state'
+import { html } from '@codemirror/lang-html'
+
+import Button from '@/components/Button.vue'
+
+const lang = new Compartment().of(html({ autoCloseTags: false }))
+const setup = [lineNumbers(), highlightActiveLineGutter()]
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="editor">
+    <div class="combo">
+      <p>Combo</p>
+      <p class="combo-number">0</p>
+    </div>
+
+    <CodeMirror :extensions="setup" :lang="lang" value="jdaiajdsia aisjdidasj" />
+
+    <div class="name">
+      <Button class="name-button" size="large">evs</Button>
+    </div>
+
+    <div class="right-bottom">
+      <div class="reference">
+        <p>Reference</p>
+        <img src="@/assets/challenge/page.jpg" />
+      </div>
+
+      <div class="buttons">
+        <Button size="small">Instructions</Button>
+        <Button size="small">Finish</Button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.editor {
+  background-color: #000000;
+
+  .combo {
+    color: #ffffff;
+    font-family: 'Press Start 2P';
+    right: 20px;
+    position: fixed;
+    text-align: center;
+    top: 20px;
+
+    &-number {
+      color: #4effa1;
+      font-size: 80px;
+      margin-top: 30px;
+    }
+  }
+
+  .name {
+    &-button {
+      bottom: 20px;
+      left: 20px;
+      position: absolute;
+    }
+  }
+
+  .right-bottom {
+    bottom: 20px;
+    position: absolute;
+    right: 20px;
+
+    .reference {
+      align-items: flex-end;
+      display: flex;
+      flex-flow: column;
+
+      p {
+        font-family: 'Press Start 2P';
+        font-size: 12px;
+        margin-bottom: 15px;
+        text-align: right;
+      }
+
+      img {
+        width: 200px;
+      }
+    }
+
+    .buttons {
+      display: flex;
+      gap: 10px;
+      margin-top: 10px;
+    }
+  }
+
+  :deep(.cm-editor) {
+    font-size: 20px;
+    height: 100vh;
+  }
+
+  :deep(.cm-gutters) {
+    background-color: #000000;
+    border: none;
+    color: #bebebe;
+  }
+
+  :deep(.cm-gutterElement) {
+    padding: 0px 12px 0 18px;
+  }
+
+  :deep(.cm-activeLineGutter) {
+    background-color: #1a1a1a;
+  }
+
+  :deep(.cm-content) {
+    color: #ffffff;
+    caret-color: #fff;
+  }
+}
+</style>
